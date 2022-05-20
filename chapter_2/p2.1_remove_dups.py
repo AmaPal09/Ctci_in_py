37d904754f805ab6652f515b609e08f654461746
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-20 12:12:00 -0700

    FEAT: Add chapter 2, problem 1

---
+++ chapter_2/p2.1_remove_dups.py
@@ -0,0 +1,21 @@
+#
+# Remove Dups
+# Write code to remove duplicates from an unsorted linked list.
+# FOLLOW UP
+# how will you solve this problem if a temp buffer is not allowed
+#
+
+#
+# Explaination:
+#
+
+#
+# Questions:
+# 1) What about nodes with " "?
+#
+
+#
+# Assumption:
+# 1) Function is passed the head of the linked list.
+# 2) Not a method on the LL class
+#
