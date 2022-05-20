49f2fdeab11469a925b44bb1373bb9bffc6f9444
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-19 17:50:25 -0700

    FEAT: Problem 8 from chapter 1

---
+++ chapter_1/p1.8_zero_matrix.py
@@ -0,0 +1,27 @@
+#
+# Zero Matrix
+# Write an algorithm such that if an element in an MxN jmatrix is 0,
+# its entire row and column are set to 0;
+#
+
+#
+# Assumption
+# All elements in the matrix are numeric
+#
+
+#
+# Example:
+# 1) [] -> []
+# 2) [[]] -> []
+# 3) [[1], [2,3]] -> [[1], [2,3]]
+# 4) [
+# 	[1 , 2 , 3 , 4 , 5 ],
+# 	[6 , 7 , 8 , 9 , 10],
+# 	[11, 12, 0 , 14, 15]
+# ]  -> [
+# 	[1 , 2 , 0 , 4 , 5 ],
+# 	[6 , 7 , 0 , 9 , 10],
+# 	[0 , 0 , 0 , 0 , 0 ]
+# ]
+#
+
