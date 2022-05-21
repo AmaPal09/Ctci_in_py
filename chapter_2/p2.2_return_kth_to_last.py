19c566ba7249ce06942a35a12a926fcf21328f6d
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-20 17:21:03 -0700

    CHNG: Test sol and add test cases

--- chapter_2/p2.2_return_kth_to_last.py
+++ chapter_2/p2.2_return_kth_to_last.py
@@ -35,7 +35,8 @@
 # 2) 'a'->'e'->'c'->'d'->'c'->'a', 4 => 'c'->'d'->'c'->'a'
 # */
 
-from linked_list.py import LinkedList
+from linked_list import LinkedList
+import unittest
 
 def return_kth_to_last(ll, k):
 	if len(ll) < k:
@@ -46,10 +47,42 @@ def return_kth_to_last(ll, k):
 	k_displace_node = curr_node
 
 	for i in range(k):
+		if k_displace_node == None:
+			return None
 		k_displace_node = k_displace_node.next
 
-	while k_displace_node.next:
+	while k_displace_node:
 		curr_node = curr_node.next
 		k_displace_node = k_displace_node.next
 
 	return curr_node
+
+
+ll1 = LinkedList([10, 20, 30, 40, 50])
+print(return_kth_to_last(ll1, 1))
+
+class Test(unittest.TestCase):
+	test_cases = [
+		([10, 20, 30, 40, 50], 1, 50),
+		([10, 20, 30, 40, 50], 3, 30),
+		([10,20], 3, None),
+		([], 0, None),
+		([10, 20, 30, 40, 50], 5, 10)
+	]
+
+	test_functions = [
+		return_kth_to_last,
+	]
+
+
+	def test_return_kth_to_last(self):
+		for f in self.test_functions:
+			for case, k, expected in self.test_cases:
+				result_node = return_kth_to_last(LinkedList(case), k)
+				if expected == None:
+					assert result_node == expected
+				else:
+					assert result_node.value == expected
+
+if __name__ == "__main__":
+	unittest.main()
