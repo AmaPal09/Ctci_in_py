d8dcb770c32c1ac832fb8c487cbf35c9c29bb790
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-25 10:07:41 -0700

    Test sol for problem 4 and add test cases

--- chapter_2/p2.4_partition.py
+++ chapter_2/p2.4_partition.py
@@ -28,6 +28,7 @@
 # 3) '6->3->1' [partition = 3]
 
 from linked_list import ListNode, LinkedList
+import unittest
 
 def partition_ll(head, pivot):
 
@@ -73,47 +74,67 @@ def partition_ll(head, pivot):
 		return right_head
 
 
-# '3->5->8->5->10->2->1', 5 = 3->2->1->5->8->5->10
-# '1->6->4->3->2', 3 = 1->2->6->4->3
-# '6->3->1', 2 = 1->6->3
-# '2->4->3', 1 = 2->4->3
-# '1->3->2', 4 = 1->3->2
-a1 = ListNode(3,ListNode(5, ListNode(8, ListNode(5, ListNode(10, ListNode(2, ListNode(1)))))))
-ll1 = LinkedList()
-ll1.head = a1
-print(ll1)
-a1 = partition_ll(a1,5)
-ll1.head = a1
-print(ll1)
+# a1 = ListNode(3,ListNode(5, ListNode(8, ListNode(5, ListNode(10, ListNode(2, ListNode(1)))))))
+# ll1 = LinkedList()
+# ll1.head = a1
+# print(ll1)
+# a1 = partition_ll(a1,5)
+# ll1.head = a1
+# print(ll1)
 
-a2 = ListNode(1, ListNode(6, ListNode(4, ListNode(3, ListNode(2)))))
-ll2 = LinkedList()
-ll2.head = a2
-print(ll2)
-a2 = partition_ll(a2,3)
-ll2.head = a2
-print(ll2)
+# a2 = ListNode(1, ListNode(6, ListNode(4, ListNode(3, ListNode(2)))))
+# ll2 = LinkedList()
+# ll2.head = a2
+# print(ll2)
+# a2 = partition_ll(a2,3)
+# ll2.head = a2
+# print(ll2)
 
-a3 = ListNode(6, ListNode(3, ListNode(1)))
-ll3 = LinkedList()
-ll3.head = a3
-print(ll3)
-a3 = partition_ll(a3,2)
-ll3.head = a3
-print(ll3)
+# a3 = ListNode(6, ListNode(3, ListNode(1)))
+# ll3 = LinkedList()
+# ll3.head = a3
+# print(ll3)
+# a3 = partition_ll(a3,2)
+# ll3.head = a3
+# print(ll3)
 
-a4 = ListNode(2, ListNode(4, ListNode(3)))
-ll4 = LinkedList()
-ll4.head = a4
-print(ll4)
-a4 = partition_ll(a4,1)
-ll4.head = a4
-print(ll4)
+# a4 = ListNode(2, ListNode(4, ListNode(3)))
+# ll4 = LinkedList()
+# ll4.head = a4
+# print(ll4)
+# a4 = partition_ll(a4,1)
+# ll4.head = a4
+# print(ll4)
 
-a5 = ListNode(1, ListNode(3, ListNode(2)))
-ll5 = LinkedList()
-ll5.head = a5
-print(ll5)
-a5 = partition_ll(a5,1)
-ll5.head = a5
-print(ll5)
+# a5 = ListNode(1, ListNode(3, ListNode(2)))
+# ll5 = LinkedList()
+# ll5.head = a5
+# print(ll5)
+# a5 = partition_ll(a5,1)
+# ll5.head = a5
+# print(ll5)
+
+
+class Test(unittest.TestCase):
+
+	test_cases = [
+		([3,5,8,5,10,2,1], 5, [3,2,1,5,8,5,10]),
+		([1,6,4,3,2], 3, [1,2,6,4,3]),
+		([6,3,1], 2, [1,6,3]),
+		([2,4,3], 1, [2,4,3]),
+		([1,3,2], 4, [1,3,2])
+	]
+
+	test_functions = [
+		partition_ll,
+	]
+
+	def test_partition_ll(self):
+		for f in self.test_functions:
+			for case, pivot, expected in self.test_cases:
+				ll1 = LinkedList(case)
+				ll1.head = f(ll1.head, pivot)
+				assert ll1.values() == expected
+
+if __name__ == "__main__":
+	unittest.main()
