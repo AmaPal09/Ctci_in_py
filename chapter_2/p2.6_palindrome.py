74f38a0777b9fb4c835afcba63f063f4338c2675
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-25 16:40:41 -0700

    Sol for problem 6

--- chapter_2/p2.6_palindrome.py
+++ chapter_2/p2.6_palindrome.py
@@ -21,8 +21,43 @@
 #
 # Example:
 #
-
-
+# 1 -> 2 -> 3 -> 4  ->
+# Null <- 1 1->2 -> 3 ->
+# Null<-1<-2
 from linked_list import ListNode, LinkedList
 
-def
+
+def reverse_ll(head):
+	prev = None
+	curr = head
+
+	while curr:
+		temp = ListNode(curr.value)
+		temp.next = prev
+		prev = temp
+		curr = curr.next
+
+	return temp
+
+
+
+def check_if_palindrome(head1, head2):
+	if not head1 or not head2:
+		return False
+
+	head1 = reverse_ll(head1)
+
+	while head1 and head2:
+		if head1.value != head2.value:
+			return False
+		head1 = head1.next
+		head2 = head2.next
+
+	if head1 and not head2:
+		return False
+	elif head2 and not head1:
+		return False
+	else:
+		return True
+
+
