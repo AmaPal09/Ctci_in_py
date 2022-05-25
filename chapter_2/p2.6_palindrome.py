0d1f6d90b128e1fb45a91e1f6733bf12853a1c31
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-25 15:51:06 -0700

    Problem 6 from chapter 2

---
+++ chapter_2/p2.6_palindrome.py
@@ -0,0 +1,28 @@
+#
+# Palindrome
+# Implement function to check if a linked list is a palindrome
+#
+
+#
+# Explaination:
+# Store reversed verion of curr LL in another LL.
+# Compare the inpout LL with the reversed LL.
+# If both match, LL is a palindrome.
+#
+
+#
+# Questions:
+#
+
+#
+# Assumption:
+#
+
+#
+# Example:
+#
+
+
+from linked_list import ListNode, LinkedList
+
+def
