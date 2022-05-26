3c96fa25d3f0e78c837a4485bd1bcea605b00ef7
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-25 22:39:40 -0700

    Add test and test cases

--- chapter_2/p2.6_palindrome.py
+++ chapter_2/p2.6_palindrome.py
@@ -25,7 +25,7 @@
 # Null <- 1 1->2 -> 3 ->
 # Null<-1<-2
 from linked_list import ListNode, LinkedList
-
+import unittest
 
 def reverse_ll(head):
 	prev = None
@@ -61,3 +61,42 @@ def check_if_palindrome(head1, head2):
 		return True
 
 
+class Test(unittest.TestCase):
+	test_cases = [
+		([1,2,3], [3,2,1], True),
+		([1,2,3,4], [2,1,3,4], False),
+	]
+
+	test_functions = [
+		check_if_palindrome,
+	]
+
+	def test_check_if_palindrome(self):
+		for f in self.test_cases:
+			for h1, h2, expected in self.test_cases:
+				head1_ll = LinkedList(h1)
+				head2_ll = LinkedList(h2)
+
+				assert check_if_palindrome(head1_ll.head, head2_ll.head) == expected
+
+
+class Test2(unittest.TestCase):
+	test_cases = [
+		([1,2,3,4], [4,3,2,1]),
+	]
+
+	test_functions = [
+		reverse_ll
+	]
+
+	def test_reverse_ll(self):
+		for f in self.test_cases:
+			for case, expected in self.test_cases:
+				case_ll = LinkedList(case)
+				result = LinkedList()
+				result.head = reverse_ll(case_ll.head)
+				assert result.values() == expected
+
+
+if __name__ == "__main__":
+	unittest.main()
