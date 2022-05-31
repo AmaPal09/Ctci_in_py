20cdd3d2f4d801d4393d72f4dfac11e43f33d2ed
Amarja Palekar <amarjapalekar09@gmail.com>
2022-05-31 13:56:37 -0700

    Add solution for problem 7 of chapter 2

--- chapter_2/p2.7_intersection.py
+++ chapter_2/p2.7_intersection.py
@@ -32,7 +32,64 @@
 # Example:
 #
 
+from linked_list import ListNode, LinkedList
 
+
+def Result:
+	def __init__(node, size):
+		self.tail = node
+		self.size = size
+
+
+def get_tail_n_size(head):
+	curr = head
+	size =
+
+	while curr.next:
+		size +=	1
+		curr = curr.next
+
+	return Result(curr, size)
+
+
+def jump_to_Kth_node(head, k):
+	curr = head
+
+	while k > 0 and curr:
+		curr = curr.next
+		k -= 1
+
+	return curr
+
+
+def get_intersecting_node(head1, head2);
+
+	if not head1 or not head2:
+		return None
+
+	curr1 = head1
+	curr2 = head2
+
+	result1 = get_tail_n_size(curr1)
+	result2 = get_tail_n_size(curr2)
+
+	if result1.tail != result2.tail:
+		return None
+
+	if result1.size > result2.size:
+		longer = head1
+		shorter = head2
+	else:
+		longer = head2
+		shorter = head1
+
+	longer = jump_to_Kth_node(longer, abs(result1.size-result2.size))
+
+	while longer != shorter:
+		longer = longer.next
+		shorter = shorter.next
+
+	return longer
 
 
 
