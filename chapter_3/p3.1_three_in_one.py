3dcb0453842c05f859efb61a7f97cb5d70aa3dcf
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-01 18:04:39 -0700

    Problem 1 from chapter 1

---
+++ chapter_3/p3.1_three_in_one.py
@@ -0,0 +1,30 @@
+#
+# Three in one
+# Describe how you could use a single array to implement three stacks
+#
+
+#
+# Explaination:
+# [
+# 	top
+# 	middletop
+# 	middlebottom
+# 	bottom
+# ]
+#
+
+#
+# Questions:
+# 1)
+#
+
+#
+# Assumption:
+# 1)
+# 2)
+#
+
+#
+# Example:
+# 1)
+# ß
