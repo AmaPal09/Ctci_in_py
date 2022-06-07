39af3c2e40d98eb5479a331a94b2287dfbf0ac62
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-07 16:03:33 -0700

    Test sol for problem 2

--- chapter_3/p3.2_Stack_min.py
+++ chapter_3/p3.2_Stack_min.py
@@ -25,6 +25,8 @@
 #
 #
 
+import unittest
+
 
 class Stack:
 	def __init__(self):
@@ -48,23 +50,79 @@ class Stack:
 		else:
 			return None
 
+
 class MinStack(Stack):
 	def __init__(self):
-		super.__init__()
+		super().__init__()
 		self.minStack = Stack()
 		self.minVal = None
 
 	def push(self, val):
-		super.push(val)
-		if self.minVal < val or self.minVal == None:
+		super().push(val)
+		if self.minVal == None or self.minVal >= val :
 			self.minStack.push(val)
 			self.minVal = val
 
 	def pop(self):
-		popped = super.pop()
+		popped = super().pop()
 		if popped == self.minVal:
 			self.minStack.pop()
 			self.minVal = self.minStack.peek()
 
 	def minimum(self):
 		return self.minVal
+
+
+class Test(unittest.TestCase):
+	def test_MinStack(self):
+		test_Stack1 = MinStack()
+		assert test_Stack1.minimum() == None
+		print(test_Stack1.minStack.stack)
+
+		test_Stack1.push(4)
+		assert test_Stack1.minimum() == 4
+		print(test_Stack1.minStack.stack)
+
+		test_Stack1.push(6)
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 4
+
+		test_Stack1.push(2)
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 2
+
+		test_Stack1.push(8)
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 2
+
+		test_Stack1.push(2)
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 2
+
+		test_Stack1.pop()
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 2
+
+		test_Stack1.pop()
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 2
+
+		test_Stack1.pop()
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 4
+
+		test_Stack1.pop()
+		print(test_Stack1.minStack.stack)
+		print(test_Stack1.stack)
+		assert test_Stack1.minimum() == 4
+
+
+if __name__ == "__main__":
+	unittest.main()
