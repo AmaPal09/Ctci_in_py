ef69e828850bbee967ecaa7789adc8b7ce63546c
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-09 21:39:53 -0700

    Solution for problem 3 chapter 3

--- chapter_3/p3.3_stack_of_plates.py
+++ chapter_3/p3.3_stack_of_plates.py
@@ -32,3 +32,55 @@
 # 1)
 #
 
+from stack import Stack
+
+class Stack_of_Plates:
+	def __init__(self, capacity):
+		self._stack = []
+		self._capacity = capacity
+
+
+	def is_Empty(self):
+		return len(self._stack) == 0
+
+	def get_last_stack(self):
+		if self.is_Empty():
+			return None
+		else:
+			return self._stack[-1]
+
+	def push(self, val):
+		last_stack = self.get_last_stack()
+		if last_stack == None or last_stack.size() == self._capacity:
+			new_stack = Stack()
+			new_stack.push(val)
+			self._stack.append(new_stack)
+		else:
+			last_stack.push(val)
+
+	def pop(self):
+		last_stack = self.get_last_stack()
+		if last_stack == None:
+			return None
+		else:
+			val = last_stack.pop()
+			if len(last_stack) == 0:
+				self._stack.pop()
+			return val
+
+	def peek(self):
+		last_stack = self.get_last_stack()
+		if last_stack == None:
+			return None
+		else:
+			last_stack.peek()
+
+	def peek_at_index(self, index):
+		val = self._stack[index].pop()
+		if self._stack[index].size() == 0:
+			self._stack.pop(index)
+		return val
+
+
+
+
