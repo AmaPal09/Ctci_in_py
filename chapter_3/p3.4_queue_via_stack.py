ea438d2653cd4651c76ca12ecb4805d261add61e
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-14 15:20:49 -0700

    Solution for problem 4

--- chapter_3/p3.4_queue_via_stack.py
+++ chapter_3/p3.4_queue_via_stack.py
@@ -22,3 +22,41 @@
 # Example:
 # 1)
 #
+
+from stack import Stack
+
+class MyQueue():
+	def __init__(self):
+		self.newStack = Stack()
+		self.oldStack = Stack()
+
+	def push(self, val):
+		self.newStack.push(val)
+
+	def loadOldStack(self):
+		while (not self.newStack.isEmpty()):
+			self.oldStack.push(self.newStack.pop())
+
+	def peek(self):
+		if self.oldStack.isEmpty():
+			self.loadOldStack()
+		return self.oldStack.peek()
+
+	def pop(self):
+		if self.oldStack.isEmpty():
+			self.loadOldStack()
+		return self.oldStack.pop()
+
+mQ1 = MyQueue()
+for i in range(5):
+	mQ1.push(i)
+print(mQ1.newStack._stack)
+print(mQ1.peek())
+print(mQ1.pop())
+print(mQ1.push(7))
+print(mQ1.newStack._stack)
+print(mQ1.peek())
+print(mQ1.pop())
+print(mQ1.pop())
+print(mQ1.pop())
+print(mQ1.newStack._stack)
