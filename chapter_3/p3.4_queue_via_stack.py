c50bc111bc2e3773519acca7c020d378d7eaed2b
Amarja Palekar <amarjapalekar09@gmail.com>
2022-06-14 15:13:07 -0700

    Add problem 4 from chapter 3

---
+++ chapter_3/p3.4_queue_via_stack.py
@@ -0,0 +1,24 @@
+#
+# Queue via Stacks
+# Implement a MyQueue class which implements a queue using two stacks.
+#
+
+#
+# Explaination:
+#
+
+#
+# Questions:
+# 1)
+#
+
+#
+# Assumption:
+# 1)
+# 2)
+#
+
+#
+# Example:
+# 1)
+#
